cb572d72c463963c7b2f9e404db8b62d4815e2a1
jeel-kankadiya <24aiml016@charusat.edu.in>
2026-08-17 20:40:32 +0530

    task manager

--- Backhand-24AIML016/jeel -api/mongoDB_24AIML016/server_new.js
+++ Backhand-24AIML016/jeel -api/mongoDB_24AIML016/server_new.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const Task = require('./models/Task');
@@ -11,6 +12,7 @@ const app = express();
 // MIDDLEWARE
 // ==========================================
 
+app.use(cors());
 app.use(express.json());
 
 
@@ -259,7 +261,7 @@ app.use((err, req, res, next) => {
 // START SERVER
 // ==========================================
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 
